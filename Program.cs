e059ceb711c930ce3a8476ea961d36808fbdcc2d
kaito <K.Bolliger.inf24@stud.bbbaden.ch>
2025-09-01 11:04:28 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Solo_Leveling.Domain;
+using Solo_Leveling.UI;
+
+namespace Solo_Leveling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var player = new Player("Rice");
+            Menu.ShowMain(player);
+
+
+        }
+    }
+}
